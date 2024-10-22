fca89423d091fd8eeca23555960d4f97fb806410
msudipta888 <122991182+msudipta888@users.noreply.github.com>
2024-10-22 15:17:56 +0530

    Update router.js

--- server/router.js
+++ server/router.js
@@ -2,13 +2,11 @@ const express = require("express");
 const { getNearByPlaces } = require("./index.js");
 const { getRoute } = require("./maproute.js");
 const router = express.Router();
-const newAccessToken = router.post("/generate",require("./middleware/refreshToken.js"));
 const Route = router.post("/routing", require("./middleware/auth.js"), getRoute);
-
 const nearBy = router.post(
   "/location",
   require("./middleware/auth.js"),
   getNearByPlaces
 );
 
-module.exports = { nearBy, Route,newAccessToken };
+module.exports = { nearBy, Route };
