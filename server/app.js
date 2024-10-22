64edc64de234c13f5fee7fda0f3ecbfc46d4f385
msudipta888 <122991182+msudipta888@users.noreply.github.com>
2024-10-22 15:17:08 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
  const {nearBy,Route, newAccessToken} = require('./router')
 
 const server = createServer(app);
@@ -27,15 +26,10 @@ mongoose
     console.error("Failed to connect to MongoDB:", err.message);
   });
 
-  
-app.use(cors());
-
 app.use(
  cors({
     origin: "https://mapquestorapp.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
 
@@ -43,8 +37,6 @@ const io = socketIo(server, {
  cors({
     origin: "https://mapquestorapp.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, 
   })
 });
   app.use(express.json());
@@ -61,7 +53,6 @@ app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`);
   next();
 });
-app.use("/refresh-token",newAccessToken);
 app.use("/map",Route)
 app.use("/nearby-places", nearBy);
 app.use("/users", require("./routes/authRoutes"));
