400241bb07cb4e5ba9ba04627e6af1e6c9144a61
msudipta888 <122991182+msudipta888@users.noreply.github.com>
2024-10-22 10:33:43 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -32,8 +32,9 @@ mongoose
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: "https://mapquestorapp.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
     credentials: true,
   })
 );
